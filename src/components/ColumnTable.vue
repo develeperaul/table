c9f2ddd36acabfd682c87fcec771fb3774bf3c83
Ralfik <developeraul@gmail.com>
2021-08-17 08:22:10 +0500

    колонка

---
+++ src/components/ColumnTable.vue
@@ -0,0 +1,32 @@
+<template>
+<!-- колонна таблицы -->
+    <div
+
+      class="tw-flex tw-flex-col "
+      :class="`tw-w-1/3`"
+      :style="[{background:(index+1)%2==0 ? ' rgba(196, 196, 196, 0.05)' : ''}, {paddingLeft:index>0 ? '30px': ''}]"
+    >
+
+    <slot></slot>
+
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+      required: true,
+    },
+    item: {
+      type: Object,
+      required: true,
+    }
+  },
+  // name: 'ComponentName',
+  data () {
+    return {}
+  }
+}
+</script>
