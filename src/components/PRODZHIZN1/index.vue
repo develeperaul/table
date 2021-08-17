0bddc885e2da370d71aaecc92e2ab2bfb2c535f6
Ralfik <developeraul@gmail.com>
2021-08-17 09:17:28 +0500

    table PRODZHIZN1

---
+++ src/components/PRODZHIZN1/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="tw-flex">
+        <ColumnTable
+      v-for="(item, index) in json"
+      :key="index"
+      v-bind="{index, item}"
+    > 
+      <span
+        class="date">
+        {{item.date}}
+      <BorderLine
+      style="margin-top: 25px"
+      :style="{marginLeft:[index>0 ? '-30px': '']}"/>
+      </span>
+
+      <MainNumber color="green" text="46541651" mb="35" after="план"/>  
+      <MainNumber color="green" text="46541651" mb="35" execution="failed" after="факт"/>  
+
+      <div class="tw-w-full tw-relative">
+        <TableGradientLine v-if="index<1" colorGradient="green"
+        text="Миграционный прирост"
+        class="tw-absolute"
+      :style="{width: `${100*json.length}%`}" 
+        />
+      </div>
+      <MainNumber  color="red" text="-11111" mt="130" mb="75"/>  
+
+      <div class="tw-w-full tw-relative">
+        <TableGradientLine v-if="index<1" colorGradient="green"
+        text="Миграционный прирост"
+        class="tw-absolute"
+      :style="{width: `${100*json.length}%`}" 
+        />
+      </div>
+      <MainNumber  color="red" text="65489" mt="130" mb="75"/>   
+    </ColumnTable>
+
+  </div>
+</template>
+
+<script>
+import ColumnTable from "components/ColumnTable";
+import MainNumber from "components/MainNumber";
+import BorderLine from "components/BorderLine";
+import TableGradientLine from "components/TableGradientLine";
+
+export default {
+  // name: 'ComponentName',
+  components: {
+    ColumnTable,
+    MainNumber,
+    BorderLine,
+    TableGradientLine
+  },
+
+  data () {
+    return {
+      json: [
+        {
+          date: 2018,
+          title: 4051005,
+        },
+        {
+          date: 2018,
+          title: 4051005,
+        },
+        {
+          date: 2018,
+          title: 4051005,
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,32 @@
 <template>
   <!-- <Table colorTitle="#FF0000" colorGradient="green"/> -->
+  <div>
+
   <NAS1 />
+  <!-- <PRODZHIZN1/> -->
+  </div>
 </template>
 
 <script>
-import Table from "components/Table"
-import NAS1 from "components/NAS1"
+import Table from "components/Table";
+import NAS1 from "components/NAS1";
+import PRODZHIZN1 from "components/PRODZHIZN1"
 export default {
   name: 'MainLayout',
   components: {
     Table,
-    NAS1
+    NAS1,
+    PRODZHIZN1
   }
 }
 </script>
-<style lang="scss" scoped>
-  
+<style lang="scss" >
+    .date {
+    font-size: 50px;
+    font-weight: 700;
+    line-height: 68.09px;
+    margin-bottom: 75px;
+    
+    margin-top: 21px;
+  }
 </style>
