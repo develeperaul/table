6371a2e96859a6540b595ec20ce8e04a4369e828
Ralfik <developeraul@gmail.com>
2021-08-17 08:23:19 +0500

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,16 @@
 <template>
-  <Table colorTitle="#FF0000"/>
+  <!-- <Table colorTitle="#FF0000" colorGradient="green"/> -->
+  <NAS1 />
 </template>
 
 <script>
 import Table from "components/Table"
+import NAS1 from "components/NAS1"
 export default {
   name: 'MainLayout',
   components: {
-    Table
+    Table,
+    NAS1
   }
 }
 </script>
