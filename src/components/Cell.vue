ffe058bd2cdacca92ac2976383ebba6eb9b2442d
Ralfik <developeraul@gmail.com>
2021-08-17 08:21:52 +0500

    ячейка

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,31 +1,12 @@
 <template>
+<!-- ячейка -->
   <div>
-    <div class="tw-flex tw-flex-col tw-relative container"
-    v-for="(i, indexCell) in item" :key="indexCell">
-          <span
-          v-if="index<1"
-          class="tw-absolute title"
-          style="white-space: nowrap;">
-            {{i.title}}
-          </span>
-          
-          <span class=" subtitle"
-          >
-            {{i.subtitle}}
-            
-          </span>
-          <div
-          v-if="(indexCell+1)<item.length"  
-          style="border-bottom: 4px solid #6A7186"
-          :style="{marginLeft:[index>0 ? '-30px': '']}">
-            
-          </div>
-        
-    </div>
+    <CellNumber v-bind="{item, indexCol: index}"/>
   </div>
 </template>
 
 <script>
+import CellNumber from "components/CellNumber"
 
 export default {
   props: {
@@ -40,6 +21,10 @@ export default {
 
   },
   // name: 'ComponentName',
+  components: {
+    CellNumber
+
+  },
   data () {
     return {
       json: [
@@ -53,19 +38,6 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  .container {
-    margin-bottom: 20px;
-  }
-  .title {
-    
-    font-size: 27px;
-    line-height: 27px;
-    font-weight: 400;
-  }
-  .subtitle {
-    margin: 40px 0 20px 0;
-    font-size: 60px;
-    line-height: 60px;
-    font-weight: 700;
-  }
+
+
 </style>
