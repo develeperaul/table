cff65cfe369992d1de8cadda903324bdaff39e28
Ralfik <developeraul@gmail.com>
2021-08-17 08:21:29 +0500

    нижняя линия

---
+++ src/components/BorderLine.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+  style="border-bottom: 4px solid #6A7186"
+  >
+    
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {}
+  }
+}
+</script>
