679cc273eaa7d9df9f8395aa84da1622da4bcb12
Ralfik <developeraul@gmail.com>
2021-08-17 08:22:36 +0500

    градиентная линия

---
+++ src/components/TableGradientLine.vue
@@ -0,0 +1,42 @@
+<template>
+<!-- градиентная линия -->
+  <div
+      class=" tw-flex tw-items-center  "
+      style="width: 100%; height: 80px;"  
+      :style="{ background:[colorGradient=='green'? 'linear-gradient(270deg, #00CD77 0%, #6A7186 41.01%)' : colorGradient=='red' ? 'linear-gradient(270deg, #F30101 0%, #6A7186 41.01%)' : '']}"
+    >
+      <span
+        class=" text-gradient"
+      >
+        {{text}}
+      </span>
+    </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  props: {
+    colorGradient: {
+      required: true,
+      type: String
+    },
+    text: {
+      required: true,
+      type: String
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .text-gradient {
+    font-size: 40px;
+    line-height: 54.47px;
+    font-weight: 400;
+    margin-left: 20px;
+    white-space: nowrap;
+  }
+</style>
