af1ea2bc46d8f7f36149fa9db1274e4ccda2f8d4
Ralfik <developeraul@gmail.com>
2021-08-17 08:22:48 +0500

    Больше цифры

---
+++ src/components/MainNumber/index.vue
@@ -0,0 +1,63 @@
+
+<template>
+<!-- Большие цифры -->
+    <div class="tw-flex tw-flex-col"
+    :style="[{marginTop: mt+'px'},{marginBottom: mb+'px'}]">
+      <span class="subtitle">
+        <slot name="after" class="subtitle"></slot>
+      </span>
+      <span class="tw-text-5xl tw-font-bold title"
+        :style="{color: [color == 'green' ? '#01F859': color == 'red' ? '#FF0000' : '']}">
+          {{text}}
+      </span>
+      <span class="subtitle"
+      style="margin-top: 25px">
+        <slot name="before"></slot>
+      </span>
+      <span>
+        <slot name="execution"></slot>
+      </span>
+      
+    </div>  
+</template>
+
+<script>
+export default {
+  
+  name: 'MainNumber',
+  props: {
+    color: {
+      required: true,
+      type: String
+    },
+    text: {
+      default: '-',
+      type: String
+    },
+    mt: {
+      default: '0',
+      type: String
+    },
+    mb: {
+      default: '0',
+      type: String
+    }
+  }, 
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+    .title {
+      font-size: 110px;
+      line-height: 110px;
+      font-weight: 700;
+      
+    }
+    .subtitle {
+      font-size: 40px;
+      line-height: 40px;
+      font-weight: 600;
+    }
+</style>
